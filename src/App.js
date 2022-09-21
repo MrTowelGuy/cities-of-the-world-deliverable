7d2b47bb039bfcb589dbacb2f3a5807499c644e1
Jaden Garrity <110641058+MrTowelGuy@users.noreply.github.com>
2022-09-21 14:27:38 -0400

    mvp completed for deliverable

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-// IMPORT useState
-import React, { useState } from "react";
+import React, { useState } from "react"; // IMPORT useState
 import "./styles.css";
-// IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
-import imagesArr from "./imageData";
+import imagesArr from "./imageData"; // IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
 
 export default function App() {
-  // USE useState TO CREATE  [bigImage, setBigImage]
-  // const [bigImage, setBigImage] =
-  // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
+  // USE useState TO CREATE  [bigImage, setBigImage], AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
 
-  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
-  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+  const [bigImage, setBigImage] = useState(imagesArr[0].img);
 
-  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
-  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
+  const userClick = (imgUrl) => {
+    setBigImage(imgUrl);
+  }; // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL, THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+
+  const images = imagesArr.map((image, index) => {
+    // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
+    return (
+      // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
+      <img
+        className="thumb"
+        src={image.img}
+        alt={image.city}
+        key={index}
+        onClick={() => userClick(image.img)}
+      />
+    );
+  });
 
   return (
     <div className="App">
       <h1>Cities Of The World</h1>
       <div id="wrapper">
-        <div id="thumbnails">{/* RENDER THE IMAGES ARRAY  */}</div>
-        {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
-        {/* <img src="" id="bigimage" alt='bigImage'/> */}
+        <div id="thumbnails">{images}</div>
+        <img src={bigImage} id="bigimage" alt="bigImage" />
       </div>
     </div>
   );
